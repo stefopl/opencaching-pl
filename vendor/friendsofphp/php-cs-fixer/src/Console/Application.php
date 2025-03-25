6327e3049048b4fc3a98fc484b4b295c6cfdf803
stefopl <92673167+stefopl@users.noreply.github.com>
2025-03-25 22:55:48 +0100

    update php-cs-fixer configuration

--- vendor/friendsofphp/php-cs-fixer/src/Console/Application.php
+++ vendor/friendsofphp/php-cs-fixer/src/Console/Application.php
@@ -44,9 +44,12 @@ use Symfony\Component\Console\Output\OutputInterface;
 final class Application extends BaseApplication
 {
     public const NAME = 'PHP CS Fixer';
-    public const VERSION = '3.64.0';
-    public const VERSION_CODENAME = 'Space Sets';
+    public const VERSION = '3.73.1';
+    public const VERSION_CODENAME = 'Persian Successor';
 
+    /**
+     * @readonly
+     */
     private ToolInfo $toolInfo;
     private ?Command $executedCommand = null;
 
